d1e16ee78cd67341770fb3a674d2b5310b93e2ad
Artyr <rubkaarchi@gmail.com>
2015-09-13 21:27:35 +0200

    Initially DONE

--- Core/Src/Core/GameController.cs
+++ Core/Src/Core/GameController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using Core.Entities;
 using Core.Entities.Base;
 using Core.Entities.Interfaces;
-using Core.Entities.IslandObjects;
 using Core.Entities.RoleParameters;
 using Core.PlayerCore;
 using Core.Utils;
@@ -19,9 +18,9 @@ namespace Core.Core
 
         private readonly List<IPlayerConnection> _connections;
 
-        private int _governor;
-
         private readonly Dictionary<int, PlayerContainer> _players;
+
+        private int _governor;
 
         public int PlayersCount => _connections.Count;
 
@@ -47,11 +46,6 @@ namespace Core.Core
                     });
 
             _governor = 0;
-        }
-
-        private void InitEvents(IVisualizer visualizer)
-        {
-            OnPlayerSelectRole += visualizer.OnPlayerSelectRoleHandler;
         }
 
         public void Start()
@@ -80,7 +74,7 @@ namespace Core.Core
             RoleCard roleCard;
             //TODO: Should be in while cycle
             _mainBoardController.TryGetRoleCard(cardStatus.Role, out roleCard);
-            _players[_governor].Controller.DoRoleAction(roleCard);
+            _players[_governor].Controller.DoSelectRoleAction(roleCard);
 
             OnPlayerSelectRole?.Invoke(roleCard, _governor, currentPlayerStatus.Name);
 
@@ -151,7 +145,7 @@ namespace Core.Core
             }
             else
             {
-                _players[currentPlayerStatus.Id].Controller.DoNoPlayerAction(cardStatus.Role);
+                currentPlayerController.DoProspectorAction();
             }
 
 
@@ -263,15 +257,6 @@ namespace Core.Core
             return true;
         }
 
-        private void CheckForNextRound(MainBoardStatus status)
-        {
-            var allRoleCards = status.RoleCards;
-            if (allRoleCards.Count(x => x.IsUsed) == PlayersCount)
-            {
-                allRoleCards.ToList().ForEach(x => x.NextRound());
-            }
-        }
-
         private void DoMayorAction(bool isHasPrivilage, IPlayerConnection connection, PlayerStatus status,
             List<PlayerStatus> opponents,
             PlayerController controller)
@@ -303,6 +288,15 @@ namespace Core.Core
             bool isSuccessfull = controller.DoSelectBuildingToBuild(building, isHasPrivilage);
         }
 
+        private void CheckForNextRound(MainBoardStatus status)
+        {
+            var allRoleCards = status.RoleCards;
+            if (allRoleCards.Count(x => x.IsUsed) == PlayersCount)
+            {
+                allRoleCards.ToList().ForEach(x => x.NextRound());
+            }
+        }
+
         private List<int> GeneratePlayersOrder(int governor)
         {
             var result = new List<int> {governor};
@@ -328,195 +322,10 @@ namespace Core.Core
 
             return nextPlayer;
         }
-    }
 
-    public class PlayerController
-    {
-        private readonly PlayerStatus _playerStatus;
-
-        private readonly MainBoardController _mainBoardController;
-
-        public PlayerController(MainBoardController mainBoardController, PlayerStatus playerStatus)
+        private void InitEvents(IVisualizer visualizer)
         {
-            _mainBoardController = mainBoardController;
-            _playerStatus = playerStatus;
-        }
-
-        public Roles DoRoleAction(RoleCard card)
-        {
-            if (card.IsUsed)
-            {
-                throw new InvalidOperationException("Role card is used");
-            }
-
-            var data = card.Take();
-            _playerStatus.ReceiveDoubloons(data.Item2);
-
-            return data.Item1;
-        }
-
-        public bool DoNoPlayerAction(Roles role)
-        {
-            switch (role)
-            {
-                case Roles.Prospector:
-                    ReceiveProspectorMoney();
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        private void ReceiveProspectorMoney()
-        {
-            _playerStatus.ReceiveDoubloons(_mainBoardController.TakeDoubloons(GameConstants.ProspectorMoney));
-        }
-
-        public bool DoSelectBuildingToBuild(IBuilding building, bool isHasPrivilage)
-        {
-            var buildingToBuild =
-                _mainBoardController.Status.Buildings.Single(x => x.Key.GetType() == building.GetType());
-            var totalDiscount = _playerStatus.Board.Quarries.Count(x => x.CurrentColonistsCount > 0);
-            var realCost = buildingToBuild.Key.Cost - totalDiscount;
-
-            if (buildingToBuild.Value > 0 && realCost <= _playerStatus.Doubloons)
-            {
-                // TODO: check place amount
-                _playerStatus.Board.BuildBuilding(buildingToBuild.Key);
-                _mainBoardController.Status.Buildings[buildingToBuild.Key]--;
-
-                _mainBoardController.ReceiveDoubloons(_playerStatus.PayDoubloons(realCost));
-
-                var param = new BuilderParameters();
-                _playerStatus.Board.Buildings.OfType<BuildingBase<BuilderParameters>>()
-                    .Where(x => x.ActivePoints > 0)
-                    .ToList()
-                    .ForEach(x => x.DoAction(ref param));
-
-                if (param.TakeAdditionalColonist)
-                {
-                    _mainBoardController.Status.Colonists.Move(buildingToBuild.Key);
-                }
-
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool DoMayorActionTakeAdditionalColinists(bool usePrivilage)
-        {
-            if (usePrivilage)
-            {
-                _mainBoardController.Status.Colonists.Move(_playerStatus.Board.ColonistsWarehouse);
-
-                return true;
-            }
-
-            return true;
-        }
-
-        public bool DoMoveColonistAction(MoveDirection moveDirection)
-        {
-            moveDirection.Source.Move(moveDirection.Destination);
-
-            return true;
-        }
-
-        public bool DoSettlerAction(List<IISlandObject> islandObjects, bool isHasPrivilage)
-        {
-            var param = new SettlerParameters();
-            _playerStatus.Board.Buildings.OfType<BuildingBase<SettlerParameters>>()
-                .Where(x => x.ActivePoints > 0)
-                .ToList()
-                .ForEach(x => x.DoAction(ref param));
-
-            var tooManyObjects = islandObjects.Count() > 1 && !param.CanTakeAdditionalPlantation;
-            var notAllowedQuarry = islandObjects.OfType<Quarry>().Any() &&
-                                   !(param.CanTakeQuarryInsteadPlantation || isHasPrivilage);
-
-            if (tooManyObjects || notAllowedQuarry)
-            {
-                return false;
-            }
-
-            var quarries = islandObjects.OfType<Quarry>();
-            var plantations = islandObjects.OfType<Plantation>();
-
-            foreach (var quarry in quarries)
-            {
-                var newQuarry = _mainBoardController.Status.Quarries.Dequeue();
-                _playerStatus.Board.BuildQuarry(quarry);
-            }
-
-            foreach (var plantation in plantations)
-            {
-                _mainBoardController.Status.AvailablePlantations.Remove(plantation);
-                _playerStatus.Board.BuildPlantation(plantation);
-            }
-
-            if (param.CanTakeAdditionalColonist)
-            {
-                foreach (var islandObject in islandObjects)
-                {
-                    _mainBoardController.Status.Colonists.Move(islandObject);
-                }
-            }
-
-            return true;
-        }
-
-        public bool DoTradeAction(Goods value, bool isHasPrivilage)
-        {
-            var param = new TraderParameters();
-
-            var traderBuildings =
-                _playerStatus.Board.Buildings.OfType<BuildingBase<TraderParameters>>()
-                    .Where(x => x.ActivePoints > 0)
-                    .ToList();
-            traderBuildings.ForEach(x => x.DoAction(ref param));
-
-            if (_mainBoardController.Status.Market.CanSellGood(value, param.PermissionToSellTheSame))
-            {
-                var money = _mainBoardController.Status.Market.SellGood(value, traderBuildings);
-                if (money.HasValue)
-                {
-                    _playerStatus.Warehouse.RemoveGoods(new[] {value});
-                    _mainBoardController.Status.Doubloons -= money.Value;
-                    _playerStatus.ReceiveDoubloons(money.Value);
-
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        public void DoCaptainAction(GoodsToShip goodsToShip)
-        {
-            var goods = goodsToShip.Goods;
-            var ship = _mainBoardController.Status.Ships.Single(x => x.Space == goodsToShip.Ship.Space);
-            var playerGoodsCount = _playerStatus.Warehouse.GetGoodsCount(goods);
-            var goodsCount = Math.Min(playerGoodsCount, ship.FreeSpace);
-            ship.AddGoods(goods, goodsCount);
-            _playerStatus.Warehouse.RemoveGoods(goods, goodsCount);
-            _playerStatus.AddVp(goodsCount);
-            _mainBoardController.Status.Vp -= goodsCount;
-        }
-
-        public void DoCraftsmanAction(Dictionary<Goods, int> productions)
-        {
-            foreach (var production in productions)
-            {
-                _playerStatus.Warehouse.AddGoods(production.Key, production.Value);
-                _mainBoardController.Status.Warehouse.RemoveGoods(production.Key, production.Value);
-            }
-        }
-
-        public void DoCraftsmanActionReceiveAdditionalGoods(Goods additionalGoods)
-        {
-            _playerStatus.Warehouse.AddGoods(additionalGoods, 1);
-            _mainBoardController.Status.Warehouse.RemoveGoods(additionalGoods, 1);
+            OnPlayerSelectRole += visualizer.OnPlayerSelectRoleHandler;
         }
     }
 
---
+++ Core/Src/Core/PlayerController.cs
@@ -0,0 +1,194 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Entities;
+using Core.Entities.Base;
+using Core.Entities.Interfaces;
+using Core.Entities.IslandObjects;
+using Core.Entities.RoleParameters;
+using Core.PlayerCore;
+using Core.Utils;
+
+namespace Core.Core
+{
+    public class PlayerController
+    {
+        private readonly PlayerStatus _playerStatus;
+
+        private readonly MainBoardController _mainBoardController;
+
+        public PlayerController(MainBoardController mainBoardController, PlayerStatus playerStatus)
+        {
+            _mainBoardController = mainBoardController;
+            _playerStatus = playerStatus;
+        }
+
+        public Roles DoSelectRoleAction(RoleCard card)
+        {
+            if (card.IsUsed)
+            {
+                throw new InvalidOperationException("Role card is used");
+            }
+
+            var data = card.Take();
+            _playerStatus.ReceiveDoubloons(data.Item2);
+
+            return data.Item1;
+        }
+
+        public bool DoProspectorAction()
+        {
+            _playerStatus.ReceiveDoubloons(
+                _mainBoardController.TakeDoubloons(_mainBoardController.TakeDoubloons(GameConstants.ProspectorMoney)));
+
+            return true;
+        }
+
+        public bool DoSelectBuildingToBuild(IBuilding building, bool isHasPrivilage)
+        {
+            var buildingToBuild =
+                _mainBoardController.Status.Buildings.Single(x => x.Key.GetType() == building.GetType());
+            var totalDiscount = _playerStatus.Board.Quarries.Count(x => x.CurrentColonistsCount > 0);
+            var realCost = buildingToBuild.Key.Cost - totalDiscount;
+
+            if (buildingToBuild.Value > 0 && realCost <= _playerStatus.Doubloons)
+            {
+                // TODO: check place amount
+                _playerStatus.Board.BuildBuilding(buildingToBuild.Key);
+                _mainBoardController.Status.Buildings[buildingToBuild.Key]--;
+
+                _mainBoardController.ReceiveDoubloons(_playerStatus.PayDoubloons(realCost));
+
+                var param = new BuilderParameters();
+                _playerStatus.Board.Buildings.OfType<BuildingBase<BuilderParameters>>()
+                    .Where(x => x.ActivePoints > 0)
+                    .ToList()
+                    .ForEach(x => x.DoAction(ref param));
+
+                if (param.TakeAdditionalColonist)
+                {
+                    _mainBoardController.Status.Colonists.Move(buildingToBuild.Key);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DoMayorActionTakeAdditionalColinists(bool usePrivilage)
+        {
+            if (usePrivilage)
+            {
+                _mainBoardController.Status.Colonists.Move(_playerStatus.Board.ColonistsWarehouse);
+
+                return true;
+            }
+
+            return true;
+        }
+
+        public bool DoMoveColonistAction(MoveDirection moveDirection)
+        {
+            moveDirection.Source.Move(moveDirection.Destination);
+
+            return true;
+        }
+
+        public bool DoSettlerAction(List<IISlandObject> islandObjects, bool isHasPrivilage)
+        {
+            var param = new SettlerParameters();
+            _playerStatus.Board.Buildings.OfType<BuildingBase<SettlerParameters>>()
+                .Where(x => x.ActivePoints > 0)
+                .ToList()
+                .ForEach(x => x.DoAction(ref param));
+
+            var tooManyObjects = islandObjects.Count() > 1 && !param.CanTakeAdditionalPlantation;
+            var notAllowedQuarry = islandObjects.OfType<Quarry>().Any() &&
+                                   !(param.CanTakeQuarryInsteadPlantation || isHasPrivilage);
+
+            if (tooManyObjects || notAllowedQuarry)
+            {
+                return false;
+            }
+
+            var quarries = islandObjects.OfType<Quarry>();
+            var plantations = islandObjects.OfType<Plantation>();
+
+            foreach (var quarry in quarries)
+            {
+                var newQuarry = _mainBoardController.Status.Quarries.Dequeue();
+                _playerStatus.Board.BuildQuarry(quarry);
+            }
+
+            foreach (var plantation in plantations)
+            {
+                _mainBoardController.Status.AvailablePlantations.Remove(plantation);
+                _playerStatus.Board.BuildPlantation(plantation);
+            }
+
+            if (param.CanTakeAdditionalColonist)
+            {
+                foreach (var islandObject in islandObjects)
+                {
+                    _mainBoardController.Status.Colonists.Move(islandObject);
+                }
+            }
+
+            return true;
+        }
+
+        public bool DoTradeAction(Goods value, bool isHasPrivilage)
+        {
+            var param = new TraderParameters();
+
+            var traderBuildings =
+                _playerStatus.Board.Buildings.OfType<BuildingBase<TraderParameters>>()
+                    .Where(x => x.ActivePoints > 0)
+                    .ToList();
+            traderBuildings.ForEach(x => x.DoAction(ref param));
+
+            if (_mainBoardController.Status.Market.CanSellGood(value, param.PermissionToSellTheSame))
+            {
+                var money = _mainBoardController.Status.Market.SellGood(value, traderBuildings);
+                if (money.HasValue)
+                {
+                    _playerStatus.Warehouse.RemoveGoods(new[] {value});
+                    _mainBoardController.Status.Doubloons -= money.Value;
+                    _playerStatus.ReceiveDoubloons(money.Value);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void DoCaptainAction(GoodsToShip goodsToShip)
+        {
+            var goods = goodsToShip.Goods;
+            var ship = _mainBoardController.Status.Ships.Single(x => x.Space == goodsToShip.Ship.Space);
+            var playerGoodsCount = _playerStatus.Warehouse.GetGoodsCount(goods);
+            var goodsCount = Math.Min(playerGoodsCount, ship.FreeSpace);
+            ship.AddGoods(goods, goodsCount);
+            _playerStatus.Warehouse.RemoveGoods(goods, goodsCount);
+            _playerStatus.AddVp(goodsCount);
+            _mainBoardController.Status.Vp -= goodsCount;
+        }
+
+        public void DoCraftsmanAction(Dictionary<Goods, int> productions)
+        {
+            foreach (var production in productions)
+            {
+                _playerStatus.Warehouse.AddGoods(production.Key, production.Value);
+                _mainBoardController.Status.Warehouse.RemoveGoods(production.Key, production.Value);
+            }
+        }
+
+        public void DoCraftsmanActionReceiveAdditionalGoods(Goods additionalGoods)
+        {
+            _playerStatus.Warehouse.AddGoods(additionalGoods, 1);
+            _mainBoardController.Status.Warehouse.RemoveGoods(additionalGoods, 1);
+        }
+    }
+}
